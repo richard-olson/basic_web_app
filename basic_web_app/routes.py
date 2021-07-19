b8344140fc3330dd1ad8b4abf548ffced99d27fb
Richard Olson <mr.richard.olson@gmail.com>
2021-07-19 03:10:16 +0000

    Added load-test functionality

--- basic_web_app/routes.py
+++ basic_web_app/routes.py
@@ -2,43 +2,62 @@ from flask import (
     render_template,
     current_app as app
 )
+import random
+import logging
+import datetime
 import config
+import math
 from . import workers
 
+
 cfg = config.Config
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @app.route("/")
 def index():
 
-    app.logger.debug('Index route accessed')
-    app.logger.debug('Attempting to query database ID')
+    logger.debug('Index route accessed')
+    logger.debug('Attempting to query database ID')
     try:
         db_id = workers.get_db_id()
-        app.logger.debug('Database ID is: ' + db_id[0][1])
+        logger.debug('Database ID is: ' + db_id[0][1])
     except Exception as e:
         db_id = 'ERROR'
-        app.logger.debug(e)
+        logger.debug(e)
 
     if db_id != 'ERROR':
         db_id = db_id[0][1]
 
-    app.logger.debug('Attempting to get instance information')
-    instances = workers.get_instance_data(cfg.region_name, cfg.tags['aws:cloudformation:stack-name'])
-    app.logger.debug(f'Instance information is:')
+    logger.debug('Attempting to get instance information')
+    instances = workers.get_instance_data(
+        cfg.region_name,
+        cfg.tags['aws:cloudformation:stack-name']
+    )
+
+    logger.debug(f'Instance information is:')
     for idx, item in enumerate(instances):
-        app.logger.debug(f'{idx}:{instances[idx].items()}')
+        logger.debug(f'{idx}:{instances[idx].items()}')
 
     # Get RDS cluster based on database endpoint (minus aws region uri)
-    app.logger.debug('Attempting to get rds information')
-    database = workers.rds_instances(cfg.region_name, cfg.db_endpoint.split('.')[0])
+    logger.debug('Attempting to get rds information')
+    database = workers.rds_instances(
+        cfg.region_name,
+        cfg.db_endpoint.split('.')[0]
+    )
 
-    app.logger.debug('Attempting to get cloudwatch information')
-    cloudwatch_cpu_data = workers.get_cloudwatch_data(cfg.region_name, cfg.tags['aws:autoscaling:groupName'])
-    app.logger.debug('Cloudwatch response:')
-    app.logger.debug(cloudwatch_cpu_data)
+    # Get CloudWatch Data
+    logger.debug('Attempting to get cloudwatch information')
+    cloudwatch_cpu_data = workers.get_cloudwatch_data(
+        cfg.region_name,
+        cfg.tags['aws:autoscaling:groupName']
+    )
+    logger.debug('Cloudwatch response:')
+    logger.debug(cloudwatch_cpu_data)
 
-    app.logger.debug('Attempting to get render index.html')
+    logger.debug('Attempting to get render index.html')
     return render_template(
         'index.html',
         db_id=db_id,
@@ -47,3 +66,17 @@ def index():
         cfg=cfg,
         cloudwatch_cpu_data=cloudwatch_cpu_data
     )
+
+
+@app.route('/load-test')
+def load_test():
+
+    load_cycles = 100000
+
+    logger.info('Starting load-test function')
+    start_time = datetime.datetime.now()
+    number = 0
+    for i in range(0, load_cycles):
+        load = math.sqrt(random.randrange(1, 1000))
+        number += load
+    return str(number)
