dd5139b3e95340ccf0fda8a4bdffc09caa0f2e17
Richard Olson <mr.richard.olson@gmail.com>
2021-11-03 04:54:26 +0000

    Moved instance details to base view model

--- basic_web_app/services/home_service.py
+++ basic_web_app/services/home_service.py
@@ -1,5 +1,4 @@
 from re import search
-from flask import request
 from basic_web_app import app, db
 from basic_web_app.infrastructure import aws, instance
 
@@ -8,40 +7,38 @@ def get_localhost_data():
     return instance.Data()
 
 
-def get_ec2_data(localhost):
+def get_ec2_data(region, tags):
 
     ec2_data = aws.get_instance_data(
-        localhost.get_region(),
-        localhost.get_tags()["aws:cloudformation:stack-name"],
+        region,
+        tags["aws:cloudformation:stack-name"],
     )
 
     return ec2_data
 
 
-def get_ec2_health_data(localhost, ec2_data):
+def get_ec2_health_data(region, ec2_data):
 
     list_of_instances = [i["InstanceId"] for i in ec2_data]
-    ec2_health_data = aws.get_instance_health(localhost.get_region(), list_of_instances)
+    ec2_health_data = aws.get_instance_health(region, list_of_instances)
 
     return ec2_health_data
 
 
-def get_asg_data(localhost, ec2_data):
+def get_asg_data(region, ec2_data):
 
     for tag in ec2_data[0]["Tags"]:
         if tag["Key"] == "aws:autoscaling:groupName":
             asg_name = tag["Value"]
 
-    asg_data = aws.get_asg_details(localhost.get_region(), asg_name)
+    asg_data = aws.get_asg_details(region, asg_name)
 
     return asg_data
 
 
-def get_alb_data(localhost, asg_data):
+def get_alb_data(region, asg_data):
 
-    alb_data = aws.get_alb_target_health(
-        localhost.get_region(), asg_data["TargetGroupARNs"][0]
-    )
+    alb_data = aws.get_alb_target_health(region, asg_data["TargetGroupARNs"][0])
 
     return alb_data
 
@@ -61,20 +58,21 @@ def get_db_id_data():
     return db_id
 
 
-def get_rds_data(localhost):
+def get_rds_data(region, endpoint):
 
     rds_data = aws.rds_instances(
-        localhost.get_region(),
-        localhost.get_database_endpoint().split(".")[0],
+        region,
+        endpoint.split(".")[0],
     )
 
     return rds_data
 
 
-def get_cloudwatch_data(localhost):
+def get_cloudwatch_data(region, tags):
+    
     cloudwatch_data = aws.get_cloudwatch_data(
-        localhost.get_region(),
-        localhost.get_tags()["aws:autoscaling:groupName"],
+        region,
+        tags["aws:autoscaling:groupName"],
     )
 
     return cloudwatch_data
--- basic_web_app/viewmodels/shared/viewmodelbase.py
+++ basic_web_app/viewmodels/shared/viewmodelbase.py
@@ -3,13 +3,19 @@ from typing import Optional
 import flask
 from flask import Request
 
-from basic_web_app.infrastructure import request_dict
+from basic_web_app.infrastructure import ( request_dict, instance as localhost)
+
 
 
 class ViewModelBase:
     def __init__(self):
+        instance = localhost.Data()
         self.request: Request = flask.request
         self.request_dict = request_dict.create("")
+        self.instance_id: str = instance.get_instance_id()
+        self.region: str = instance.get_region()
+        self.tags: dict = instance.get_tags()
+        self.db_endpoint: str = instance.get_database_endpoint()
         self.error: Optional[str] = None
 
     def to_dict(self):
