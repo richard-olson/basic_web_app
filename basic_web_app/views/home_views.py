9b56a0b95c8f7bff314e6abda74c830b0096bde8
Richard Olson <mr.richard.olson@gmail.com>
2021-11-16 04:39:09 +0000

    starting to tinker with async

--- basic_web_app/views/home_views.py
+++ basic_web_app/views/home_views.py
@@ -1,14 +1,22 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_
 from basic_web_app.infrastructure.view_modifiers import response
 from basic_web_app.viewmodels.home.index_viewmodel import IndexViewModel
+from basic_web_app.viewmodels.home.async_index_viewmodel import AsyncIndexViewModel
 
 
 blueprint = Blueprint("home", __name__, template_folder="templates")
 
-@blueprint.route("/")
+@blueprint.get("/")
 @response(template_file="index.html", no_caching=True)
 def index():
 
     vm = IndexViewModel()
+
+    return vm.to_dict()
+
+@blueprint.get("/async")
+async def async_index():
+
+    vm = AsyncIndexViewModel()
 
     return vm.to_dict()
